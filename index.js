f4ed213df91e2747d7818e86e0eb4fe178231efc
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-09-01 21:05:46 +0300

    data

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config()
+//import * from 'dotenv' 
+
 const express = require('express')
+const { Pool } = require('pg')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -18,12 +23,29 @@ const books = [
 
 const port = process.env.PORT || 8080
 
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    ssl: true, 
+    ssl: { rejectUnauthorized: false }
+})
+
 app.get('/', (req, res) => {
     res.render('greeting')
 })
 
 app.get('/api/book', (req, res ) => {
-    res.json(books)
+    pool.query('SELECT * FROM books;')
+    .then((data) => {res.json(data)})
+    .catch((err) => {
+        res.status(400).send({
+            error: err.message
+        })
+    })
+    
 })
 
-app.listen(port)
+app.listen(port, () => console.log('conncted to Postgre'))
